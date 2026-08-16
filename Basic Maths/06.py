752c4a6f4653013bb228baa3f814f344bff5f17b
Abhishek bansal <222248939+Artemis003-creator@users.noreply.github.com>
2026-08-16 20:38:23 +0530

    All divisiors

--- Basic Maths/06.py
+++ Basic Maths/06.py
@@ -4,6 +4,9 @@
 
 # All factors of a number can be found till [sq_root(n)] or [i*i <=n]
 
+
+## Method 1
+
 def all_div(n):
     result = []
     i = 1
@@ -16,6 +19,28 @@ def all_div(n):
         
         i +=1
     return sorted(result)
+
+n = int(input())
+print(all_div(n))
+
+
+## Method 2  --- optimised
+
+def all_div(n):
+    smaller = []
+    larger = []
+
+    i = 1
+
+    while i*i <=n:
+        if n%i == 0 :
+            smaller.append(i)
+            if i != n//i:
+                larger.append((n//i))
+        
+        i +=1
+    result = smaller + larger[::-1]
+    return result
 
 n = int(input())
 print(all_div(n))
