2db549a1b383dd6467ded6d5c2635da53acb9d2c
Abhishek bansal <222248939+Artemis003-creator@users.noreply.github.com>
2026-08-16 19:31:28 +0530

    Medium GCD/HCF

---
+++ Basic Maths/04.py
@@ -0,0 +1,24 @@
+
+# Q4 Find GCD of two numbers
+
+# Problem Statement: Given two integers N1 and N2, find their greatest common divisor. (a,b > 0)
+
+
+
+def gcd(a,b):
+
+    while a >0 and b > 0:
+        if a>b :
+            a = a%b
+        else:
+            b = b%a
+
+    if a == 0:
+        return b
+    else:
+        return a
+
+a = 12
+b = 9
+
+print(gcd(a,b))
